b3f6edde1ef22a9b77da61b3dde2a85596158123
rosaliamnrg <100587845+rosaliamnrg@users.noreply.github.com>
2025-07-01 22:02:16 +0700

    Update upload.js
    ubah biar rata tengah (2)

--- src/pages/admin/upload.js
+++ src/pages/admin/upload.js
@@ -237,7 +237,8 @@ export default function AdminUpload() {
                             <td
                               style={{
                                 border: "1px solid #D9D9D9",
-                                padding: "3px"
+                                padding: "3px",
+                                textAlign:"center"
                               }}
                             >
                               {index + 1}
@@ -274,8 +275,7 @@ export default function AdminUpload() {
                             <td
                               style={{
                                 border: "1px solid #D9D9D9",
-                                padding: "3px",
-                                textAlign:"center"
+                                padding: "3px"
                               }}
                             >
                               <span
